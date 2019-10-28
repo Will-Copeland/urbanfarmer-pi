e79fd35e30f028e844de473e8039877a085ad6fa
Will-Copeland <will@localhost.localdomain>
2019-10-27 21:55:40 -0700

    keep fb init in index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import * as firebase from 'firebase-admin';
 
+const admin = require('firebase-admin');
 const TEST = require('./TEST');
-const initializeFirebase = require('./initializeFirebase');
+// eslint-disable-next-line import/no-unresolved
+const serviceAccount = require('../../urbanfarmer-c46e0-firebase-adminsdk-jun7f-432811d285.json');
 
+module.exports = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://urbanfarmer-c46e0.firebaseio.com',
+});
 
 function main() {
   let tempArr = [];
