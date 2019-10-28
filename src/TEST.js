ceb94560fd973e3c24c655a3aef086cca3a251a1
Will-Copeland <will@localhost.localdomain>
2019-10-27 20:13:19 -0700

    more logs

--- src/TEST.js
+++ src/TEST.js
@@ -1,8 +1,11 @@
 const { spawn } = require('child_process');
 
 module.exports = () => {
-  const process = spawn('python', ['./Python/grove_dht_pro.py']);
+  console.log('Starting child process...');
 
+  const process = spawn('python', ['./Python/grove_dht_pro.py']);
+  console.log("started!");
+  
   process.stdout.on('data', (data) => {
     console.log('Node Log with data from python: ', data.toString());
   });
