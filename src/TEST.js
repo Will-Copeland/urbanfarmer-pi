0f6232d478a24566bd21c3094f4a64dfac6a6e06
Will-Copeland <will@localhost.localdomain>
2019-10-27 19:45:18 -0700

    use grove_dht_pro.py in TEST

--- src/TEST.js
+++ src/TEST.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 module.exports = () => {
-  const process = spawn('python', ['./Python/TEST.py']);
+  const process = spawn('python', ['./Python/grove_dht_pro.py']);
 
   process.stdout.on('data', (data) => {
     console.log('Node Log with data from python: ', data.toString());
