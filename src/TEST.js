15228dde772459c6f62c89c494d955f30d6a264b
Will-Copeland <will@localhost.localdomain>
2019-10-27 22:35:52 -0700

    change import route

--- src/TEST.js
+++ src/TEST.js
@@ -4,7 +4,7 @@ const { spawn } = require('child_process');
 module.exports = () => {
   console.log('Starting child process...');
 
-  const process = spawn('python', ['./Python/grove_dht_pro.py'], '-u');
+  const process = spawn('python', ['../Python/grove_dht_pro.py', '']);
   console.log('started!');
 
   process.stdout.on('data', (data) => {
