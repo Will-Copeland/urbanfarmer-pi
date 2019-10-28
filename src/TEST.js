5c9a3a50b99dc5ef84e03e701b104daa07553ac1
Will-Copeland <will@localhost.localdomain>
2019-10-27 21:36:35 -0700

    remove text from output

--- src/TEST.js
+++ src/TEST.js
@@ -3,6 +3,7 @@ import * as firebase from 'firebase-admin';
 const { spawn } = require('child_process');
 
 module.exports = () => {
+    const tempArr = [];
   console.log('Starting child process...');
 
   const process = spawn('python', ['./Python/grove_dht_pro.py']);
@@ -10,6 +11,6 @@ module.exports = () => {
 
   process.stdout.on('data', (data) => {
     console.log('Node Log with data from python: ', data.toString());
-    firebase.firestore().collection('test').add({ yep: 'yep' });
+    
   });
 };
