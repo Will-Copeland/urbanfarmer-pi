487443b6b6a1688e99eefee7ecc9c6204c8e1844
Will-Copeland <will@localhost.localdomain>
2019-10-30 20:49:27 -0700

    .gitignore fix

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-console.log(path.join(__dirname, '..'));
-
-fs.readdirSync(path.join(__dirname, '..')).forEach((file) => {
-  console.log('file: ', file);
-});
