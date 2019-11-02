718d2d2da709f524a09d87e17e8b374b9b4664ed
Will-Copeland <will@localhost.localdomain>
2019-11-02 11:07:34 -0700

    add cron job

--- src/updateFirestore.ts
+++ src/updateFirestore.ts
@@ -1,73 +1,139 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable class-methods-use-this */
 import * as firebase from "firebase-admin";
-import fs from "fs";
-import path from "path";
+import { ITempData } from "./models/TempData";
+import schedule from "node-schedule";
 
-export interface RecordKeeper {
+export type DataType = 'tempData'; // Add data types as the get used. Next will be "soilMoisture"
+
+export interface RecordKeeperProperties {
   docID: string;
-  collection:
+  collection: string;
+  recordDate: string; // Date().toDateString();
+  createdAt: number; // unix
+  updatedAt: any; // firestore.FieldValue.serverTimestamp()
+  firstTime?: number; // unix
+  lastTime?: number; // unix
+  tempData: ITempData[];
+
 }
 
-class RecordKeeper {
-  constructor() {
-    this.docID = ""; // The document we're storing our records in for the day
-    this.collection = ""; // And the collection
+class RecordKeeper implements RecordKeeperProperties{
+  public docID!: string;
+  public collection!: string;
+  public recordDate!: string;
+  public createdAt!: number;
+  public firstTime: number | undefined; // unix
+  public lastTime: number | undefined; // unix
+  public tempData!: ITempData[];
+  public updatedAt: any;
+
+
+  constructor(collection: string) {
+    this.init(collection);
+    this.initSchedule(collection);
   }
 
-  public save() {
+  
 
-  }
-
-  public async _setCurrentDoc() {
-    const isToday = await firebase
-      .firestore()
-      .collection(this.collection)
-      .orderBy("createdAt", "desc")
-      .limit(1)
-      .get()
-      .then((resp) => {
-        resp.forEach((doc) => {
-          const data = doc.data();
-          data.id = doc.id;
-          const docDay = new Date(data.createdAt); // createdAt === unix
-          if (this._isToday(docDay)) {
-            return data;
-          }
-          return false;
-        });
-      });
-
-    if (isToday) {
-      this.docID = isToday.id;
-    }
-  }
-
-  public _isToday(date) {
+  public _isToday(date: Date) {
     const today = new Date();
     return date.getDate() === today.getDate()
       && date.getMonth() === today.getMonth()
       && date.getFullYear() === today.getFullYear();
   }
 
-  public _init() {
-    fs.readdirSync(path.join(__dirname, "../data"));
-
+ 
+  private async init(collection: string) {
+    await firebase
+      .firestore()
+      .collection(collection)
+      .orderBy('dateCreated', 'desc')
+      .limit(1)
+      .get()
+      .then(resp => {
+        resp.forEach(doc => {
+          const data = doc.data();
+          data.id = doc.id;
+          const unix = data.dateCreated;
+          if (this._isDocToday(unix)) {
+            return this._setProperties(data, collection)
+          } else {
+            return this._newDoc(collection);
+          }
+        });
+      })
   }
+
+
+  public addData(dataType: DataType, data: any) {
+    if (!this.firstTime) this.firstTime = new Date().getTime();
+    this[dataType].push(data);
+  }
+
+  public async save() {
+    const props: any = this._getProperties();
+    const noUndefined: any = {};
+    Object.keys(props).forEach((key: any) => {
+      if (!!props[key]) {
+        noUndefined[key] = props[key]
+      }
+    });
+    await firebase.firestore()
+    .collection(this.collection)
+    .doc(this.docID)
+    .update({
+      ...noUndefined,
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  }
+
+   private async _newDoc(collection: string) {
+    const data = {
+      recordDate: new Date().toDateString(),
+      tempData: [],
+      createdAt: new Date().getDate(),
+    }
+    await firebase.firestore()
+    .collection(collection)
+    .add(data)
+    .then(doc => {
+      this.docID = doc.id;
+    });
+  }
+
+  private _setProperties(props: any, collection: string) {
+    this.collection = collection;
+    this.docID = props.id;
+    this.recordDate = props.recordDate;
+  }
+
+  private _todaysDate(): string {
+    return new Date().toDateString();
+  }
+
+  private _isDocToday(docUnix: number) {
+    const docDay = new Date(docUnix).toDateString();
+    return docDay === this._todaysDate();
+  }
+
+  private _getProperties() {
+    return {
+      tempData: this.tempData,
+      firstTime: this.firstTime,
+      lastTime: this.lastTime,
+    }
+  }
+
+  private initSchedule(collection: string) {
+    const onNewDay = new schedule.RecurrenceRule();
+    onNewDay.hour = 0;
+    onNewDay.minute = 0;
+    schedule.scheduleJob(onNewDay, () => {
+      this._newDoc(collection);
+    });
+  }
+
+
+
 }
-firebase.firestore()
-  .collection("test")
-  .add({
-    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-    uploadedAt: firebase.firestore.FieldValue.serverTimestamp(),
-    first: timeArr[0],
-    last: timeArr[timeArr.length - 1],
-    nRecords: timeArr.length,
-    records: data,
-  }).then((docId) => {
-    // data = {};
-    console.log("UPDATED");
-  })
-  .catch((e) => {
-    console.log("ERROR UPDATING FS: ", e);
-  });
