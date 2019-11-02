e7d709d81151b4758ccc13d78b0abdecb7b27070
Will-Copeland <will@localhost.localdomain>
2019-11-02 11:17:49 -0700

    use RecordKeeper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import * as firebase from "firebase-admin";
+import RecordKeeper from "./RecordKeeper";
+import { ITempData } from "./models/TempData";
 
 const admin = require("firebase-admin");
-const TEST = require("./TEST");
+const readTemp = require("./readTemp");
 // eslint-disable-next-line import/no-unresolved
 const serviceAccount = require("../../urbanfarmer-c46e0-firebase-adminsdk-jun7f-432811d285.json");
 
@@ -10,38 +11,22 @@ module.exports = admin.initializeApp({
   databaseURL: "https://urbanfarmer-c46e0.firebaseio.com",
 });
 
-function main() {
-  let data: any = {};
-  TEST((temp: string, humidity: string) => {
-    const time = new Date().getTime();
-    data[time] = { temp, humidity };
-    console.log("dataObj: ", data);
+function main(Record: RecordKeeper) {
+  readTemp((temp: number, humidity: number) => {
+   const data: ITempData = {
+     temp,
+     humidity,
+     timeOfMeasurement: new Date().getTime(),
+   }
+   Record.addData('tempData', data); // pushes "data" to tempData Array
   });
-
-  setInterval(() => {
-    console.log("Sending update...");
-    const timeArr = Object.keys(data);
-    firebase.firestore()
-      .collection("test")
-      .add({
-        uploadedAt: firebase.firestore.FieldValue.serverTimestamp(),
-        first: timeArr[0],
-        last: timeArr[timeArr.length - 1],
-        nRecords: timeArr.length,
-        records: data,
-      }).then((docId) => {
-        data = {};
-        console.log("UPDATED");
-      })
-      .catch((e) => {
-        console.log("ERROR UPDATING FS: ", e);
-      });
-  }, 600000);
 }
 
 function run() {
   setInterval(() => {}, 1 << 30);
-  main();
+  const Record = new RecordKeeper('test');
+
+  main(Record);
 }
 
 run();
