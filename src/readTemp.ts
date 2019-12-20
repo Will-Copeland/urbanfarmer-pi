6870ff3f8986d4a1cddacc58208e3ae353b0b110
Will-Copeland <will@LAPTOP-ONNE4PHD.hsd1.ca.comcast.net>
2019-12-19 21:14:51 -0800

    use es6

--- src/readTemp.ts
+++ src/readTemp.ts
@@ -1,14 +1,14 @@
 const { spawn } = require("child_process");
 
-module.exports = (cb: (temp: number, humidity: number) => void) => {
+export default (cb: (temp: number, humidity: number) => void) => {
   console.log("Starting temp logging!");
 
   const process = spawn("python", ["../Python/grove_dht_pro.py"]);
   process.stdout.on("data", (data: Buffer) => {
     const str = data.toString();
     const arr = str.split(" ");
-    const [temp, humidity] = arr.map((data: any) => {
-      const Str = data.replace("\n", "");
+    const [temp, humidity] = arr.map((d: any) => {
+      const Str = d.replace("\n", "");
       return Str * 1;
     });
     cb(temp, humidity);
