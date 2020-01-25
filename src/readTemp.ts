0a3091569ce2e9b97f73adda51cec14a3fa9e464
Will-Copeland <will@localhost.localdomain>
2020-01-24 18:42:09 -0800

    use promise

--- src/readTemp.ts
+++ src/readTemp.ts
@@ -1,16 +1,21 @@
+import type { ITempData } from "./models/TempData";
+
 const { spawn } = require("child_process");
 
-export default (cb: (temp: number, humidity: number) => void) => {
+export default (): Promise<{temp: number, humidity: number}> => {
   console.log("Starting temp logging!");
 
-  const process = spawn("python", ["../Python/readTemp.py"]);
-  process.stdout.on("data", (data: Buffer) => {
-    const str = data.toString();
-    const arr = str.split(" ");
-    const [temp, humidity] = arr.map((d: any) => {
-      const Str = d.replace("\n", "");
-      return Str * 1;
+  return new Promise(resolve => {
+    const process = spawn("python", ["../Python/readTemp.py"]);
+    process.stdout.on("data", (data: Buffer) => {
+      const str = data.toString();
+      const arr = str.split(" ");
+      const [temp, humidity] = arr.map((d: any) => {
+        const Str = d.replace("\n", "");
+        return Str * 1;
+      });
+      resolve({temp, humidity});
     });
-    cb(temp, humidity);
-  });
+  })
+
 };
