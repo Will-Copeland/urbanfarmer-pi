1454321f23cdefe872351b383dc5a7d5aa2e841a
Will-Copeland <will@localhost.localdomain>
2020-02-12 22:12:45 -0800

    specify cwd

--- src/toggleRelay.ts
+++ src/toggleRelay.ts
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 
 export default (pin: number, state: 0 | 1) => {
   return new Promise((resolve, reject) => {
-    exec(`python toggleRelay.py ${pin} ${state}`, [pin, state], (err: any, stdout: any, stderr: any) => {
+    exec(`python toggleRelay.py ${pin} ${state}`, { cwd: "../Python/"}, (err: object, stdout: string, stderr: string) => {
       console.log("stdout: ", stdout, " ", typeof stdout);
       console.log("err: ", err, " ", typeof err);
       console.log("stderr: ", stderr, " ", typeof stderr);
