58768c1f08a5ed14728e3fcf68c83420257c8f26
Will-Copeland <will@localhost.localdomain>
2020-02-12 23:37:51 -0800

    b

--- src/toggleRelay.ts
+++ src/toggleRelay.ts
@@ -20,7 +20,7 @@ const { exec } = require("child_process");
 
 export default (pin: number, state: 0 | 1) => {
   return new Promise((resolve, reject) => {
-    exec(`cd /home/pi/urbanfarmer-pi/Python && python toggleRelay.py ${pin} ${state}`, (err: object, stdout: string, stderr: string) => {
+    exec(`python toggleRelay.py ${pin} ${state}`, { cwd: "../Python/"}, (err: object, stdout: string, stderr: string) => {
       console.log("stdout: ", stdout, " ", typeof stdout);
       console.log("err: ", err, " ", typeof err);
       console.log("stderr: ", stderr, " ", typeof stderr);
