f1986099a55879243ab542e788210670f03c93de
Will-Copeland <will@localhost.localdomain>
2020-02-12 22:08:13 -0800

    use exec instead of execFile to circumvent permission issue

--- src/toggleRelay.ts
+++ src/toggleRelay.ts
@@ -1,9 +1,9 @@
 
-const { execFile } = require("child_process");
+const { exec } = require("child_process");
 
 export default (pin: number, state: 0 | 1) => {
   return new Promise((resolve, reject) => {
-    execFile("../Python/toggleRelay.py", [pin, state], (err: any, stdout: any, stderr: any) => {
+    exec(`python toggleRelay.py ${pin} ${state}`, [pin, state], (err: any, stdout: any, stderr: any) => {
       console.log("stdout: ", stdout, " ", typeof stdout);
       console.log("err: ", err, " ", typeof err);
       console.log("stderr: ", stderr, " ", typeof stderr);
