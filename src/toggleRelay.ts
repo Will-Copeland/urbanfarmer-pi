4f173e565bdd85dfc430dee9a66ec1ecacce76ee
Will-Copeland <will@localhost.localdomain>
2020-02-28 18:26:24 -0800

    s

--- src/toggleRelay.ts
+++ src/toggleRelay.ts
@@ -2,18 +2,12 @@
 import { spawn } from "child_process";
 
 export default (state: 0 | 1) => {
-  return new Promise(resolve => {
-    console.log(`../Python/${state === 0 ? "relayOff" : "relayOn"}.py`);
-    const process = spawn("python", [`../Python/${state === 0 ? "relayOff" : "relayOn"}.py`]);
-    process.stdout.on("data", data => {
-      const output = data.toString();
-      console.log(output);
-
-      if (output === "Success") {
-        console.log("Node here, we done");
-        resolve();
-      }
-    })
+  console.log(`../Python/${state === 0 ? "relayOff" : "relayOn"}.py`);
+  const process = spawn("python", [`../Python/${state === 0 ? "relayOff" : "relayOn"}.py`]);
+  process.stdout.on("data", data => {
+    const output = data.toString();
+    console.log("node: ", output);
+    process.kill();
+    return;
   })
-
 }
