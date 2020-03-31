5a4b8a8ad30a3617a23c1fca7417de0d00fed1ce
Will-Copeland <will@LAPTOP-ONNE4PHD.hsd1.ca.comcast.net>
2020-03-30 21:04:18 -0700

    Added Inputs.ts
    - Currently only has DHTSensor with get status(): {
       temp: number,
       humidity: number
     }

---
+++ src/models/Inputs.ts
@@ -0,0 +1,32 @@
+import { spawn } from "child_process";
+
+export interface DHTSensorProps {
+  ioPort: number;
+}
+
+export class DHTSensor implements DHTSensorProps {
+  constructor({ ioPort }: DHTSensorProps) {
+    this.ioPort = ioPort;
+  }
+
+  public ioPort: number;
+
+  get status(): Promise<{ temp: number, humidity: number}> {
+    return new Promise(resolve => {
+      const process = spawn("python", [`../Python/readTemp.py ${this.ioPort}`]);
+      process.stdout.on("data", (data: Buffer) => {
+        const str = data.toString();
+        const arr = str.split(" ");
+        const [temp, humidity] = arr.map((d: any) => {
+          const Str = d.replace("\n", "");
+          return Str * 1;
+        });
+
+        resolve({
+          temp,
+          humidity
+        })
+      })
+    })
+  }
+}
